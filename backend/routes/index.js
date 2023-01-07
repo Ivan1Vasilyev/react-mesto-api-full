acfcf0a9d130abf46ce12534755565b4676b15c2
Ivan Vasilyev <Nalivan3747@yandex.ru>
2023-01-07 22:42:00 +0300

    удалил краш тест и проверку jwt

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,15 +3,8 @@ const userRouter = require('./users');
 const cardRouter = require('./cards');
 const loginRouter = require('./login');
 const auth = require('../middlewares/auth');
-const testJwt = require('../middlewares/test-jwt');
 
-router.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
 router.use(loginRouter);
-router.use(testJwt);
 router.use(auth);
 router.use('/cards', cardRouter);
 router.use('/users', userRouter);
