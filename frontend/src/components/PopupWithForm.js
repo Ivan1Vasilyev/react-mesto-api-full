e40646b278876ea0dddddf04052fda2bfba4d905
Ivan Vasilyev <Nalivan3747@yandex.ru>
2023-01-08 21:06:22 +0300

    fix: блокировка кнопки конфирм-попапа

--- frontend/src/components/PopupWithForm.js
+++ frontend/src/components/PopupWithForm.js
@@ -17,7 +17,7 @@ const PopupWithForm = (props) => {
         <button
           className={`form__submit-button ${props.disabled && 'form__submit-button_disabled'}`}
           type="submit"
-          disabled={textLoading ? true : props.disabled}
+          disabled={textLoading ? false : props.disabled}
         >
           {textLoading ? textLoading : props.buttonText || 'Сохранить'}
         </button>
