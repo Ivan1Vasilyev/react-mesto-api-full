ccd78816eff87a1bbfba3b565208bc6fb4796ece
Ivan Vasilyev <Nalivan3747@yandex.ru>
2023-02-27 19:49:48 +0300

    test new register form

--- frontend/src/components/FormLogin.js
+++ frontend/src/components/FormLogin.js
@@ -5,10 +5,13 @@ import useForm from '../hooks/useForm';
 import Field from './Field';
 import { ROUTE_SIGN_IN } from '../utils/constants';
 
-const FormLogin = props => {
+const FormLogin = ({ isRegister, ...props }) => {
   const textLoading = useContext(PopupOnLoadContext);
+  const formData = isRegister
+    ? { email: '', password: '', nameReg: '', aboutReg: '', avatarReg: '' }
+    : { email: '', password: '' };
 
-  const { formik, disabled } = useForm({ email: '', password: '' }, props.onSubmit);
+  const { formik, disabled } = useForm(formData, props.onSubmit);
 
   return (
     <div className="form-login">
@@ -27,16 +30,46 @@ const FormLogin = props => {
           className="form-login__input"
           type="password"
           placeholder="Password"
-          autoComplete={props.isRegister ? 'new-password' : 'current-password'}
+          autoComplete={isRegister ? 'new-password' : 'current-password'}
           name="password"
           formik={formik}
           errorClass="form-login"
         />
+        {isRegister && (
+          <>
+            <Field
+              className="form-login__input"
+              type="text"
+              placeholder="Имя"
+              autoComplete="name"
+              name="nameReg"
+              formik={formik}
+              errorClass="form-login"
+            />
+            <Field
+              className="form-login__input"
+              type="text"
+              placeholder="О себе"
+              name="aboutReg"
+              formik={formik}
+              errorClass="form-login"
+            />
+            <Field
+              className="form-login__input"
+              type="url"
+              placeholder="Ссылка на аватар"
+              autoComplete="url"
+              name="avatarReg"
+              formik={formik}
+              errorClass="form-login"
+            />
+          </>
+        )}
         <button className="form-login__submit-button" type="submit" disabled={disabled}>
           {textLoading ? textLoading : props.buttonText}
         </button>
       </form>
-      {props.isRegister && (
+      {isRegister && (
         <p className="form-login__caption">
           {'Уже зарегистрированы? '}
           <Link to={ROUTE_SIGN_IN} className="form-login__link">
