088f50775876d66ab6eb85586661f2ee16d96664
Ivan Vasilyev <Nalivan3747@yandex.ru>
2023-03-25 12:46:30 +0300

    merge scss

--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import { Route, Redirect } from 'react-router-dom';
-import { ROUTE_SIGN_IN } from '../utils/constants';
-
-const ProtectedRoute = ({ component: Component, ...props }) => (
-  <Route>{props.loggedIn ? <Component {...props} /> : <Redirect to={ROUTE_SIGN_IN} />}</Route>
-);
-
-export default ProtectedRoute;
